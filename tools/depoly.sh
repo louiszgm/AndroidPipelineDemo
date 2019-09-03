00cd5c911c1fdf06ce76b38bae8a22796ab30f8e
louiszgm <louiszgm@gmail.com>
2019-09-03 21:32:57 +0800

    s

--- tools/depoly.sh
+++ tools/depoly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -F "file=@${env.WORKSPACE}/app/build/outputs/apk/debug/app-debug.apk" \
+curl -F "file=@../app/build/outputs/apk/debug/app-debug.apk" \
 -F "uKey=476bf332dd7fd56f39621e2f34150643" \
 -F "_api_key=56c3943fcc20fe8788918a0a0695ef0a" \
 https://www.pgyer.com/apiv1/app/upload
